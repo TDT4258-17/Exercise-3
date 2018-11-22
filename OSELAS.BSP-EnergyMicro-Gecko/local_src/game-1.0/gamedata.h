e283cf8ed1daa8553a2c2ac23e3f2984ccdeb258
DennisNTNU <dennisdl@stud.ntnu.no>
2018-11-22 16:02:37 +0100

    Sprite drawing proof of conecpt

---
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/gamedata.h
@@ -0,0 +1,8 @@
+#ifndef GAMEDATA_H
+#define GAMEDATA_H
+
+#define RGB_TO_16BIT(r, g, b) (((r & 0x1f) << 11) + ((g & 0x3f) << 5) + (b & 0x1f))
+
+//extern const char* const sprite0;
+
+#endif
---
+++ spriteConversionTool/main.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	std::fstream file;
+	std::fstream ofile;
+	file.open("Untitled.txt", std::ios::in );
+	ofile.open("test_tile.txt", std::ios::out );
+
+	std::string string;
+	std::string ostring;
+
+	file >> string;
+
+	unsigned int spriteNumber = 0;
+	ofile << "unsigned short* sprite" << spriteNumber << " = {\t";
+
+	unsigned short rgb = 0;
+	unsigned int size = string.size();
+	for (int i = 0; i < size; i += 3)
+	{
+		rgb = ((((unsigned char)string[i])/8));
+		rgb = rgb << 6 | (((unsigned char)string[i+1])/4);
+		rgb = rgb << 5 | (((unsigned char)string[i+2])/8);
+		ofile << rgb;
+
+
+		if (i < size - 4)
+			ofile << ", ";
+		else
+			std::cout << "Length of string: " << i+3 << "\n";
+
+		if ((i+3) % 15 == 0)
+			ofile << "\n\t\t\t\t\t\t\t";
+	}
+
+	ofile << "};";
+
+	file.close();
+	ofile.close();
+
+	return 0;
+}
