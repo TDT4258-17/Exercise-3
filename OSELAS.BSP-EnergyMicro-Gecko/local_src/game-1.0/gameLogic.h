9e62f6ea8bd2f2a11a77acc881ac8860c65cfa30
DennisNTNU <dennisdl@stud.ntnu.no>
2018-11-25 14:47:41 +0100

    Removed a bit of now unnecessary stuff

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/gameLogic.h
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/gameLogic.h
@@ -11,15 +11,12 @@ void readButtons(unsigned char gpio);
 void changeMap(unsigned char dir);
 
 
-//void movePlayerDiscrete(signed char dx, signed char dy);
-
 void acceleratePlayer(signed char ax, signed char ay);
 void playerUpdate();
 
 
 void drawMap();
 void clearScreen();
-//void drawPlayerDiscrete();
 void drawPlayer();
 
 #endif
