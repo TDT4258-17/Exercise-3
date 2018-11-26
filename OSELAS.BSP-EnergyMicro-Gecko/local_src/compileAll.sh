84a483709bc3ddbe706d94be2e77dc5b7883ae44
ConnorBoland <tdt4258@tdt4258-VirtualBox.(none)>
2018-11-26 17:35:24 +0100

    Combined ./compile.sh and ./installAndUpload.sh for drivers and game

---
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/compileAll.sh
@@ -0,0 +1,7 @@
+cd ..
+ptxdist clean driver-gamepad
+ptxdist compile driver-gamepad
+ptxdist clean driver-sound
+ptxdist compile driver-sound
+ptxdist clean game
+ptxdist compile game
---
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/installAndUpload.sh
@@ -0,0 +1,7 @@
+cd ..
+ptxdist targetinstall driver-gamepad
+ptxdist targetinstall driver-sound
+ptxdist targetinstall game
+ptxdist targetinstall kernel
+ptxdist image root.romfs
+ptxdist test flash-rootfs
